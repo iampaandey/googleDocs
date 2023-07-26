1a77aaae2572ba6a1cf2b0b419c8b8b39f06568b
Raghav Pandey <90841468+iampaandey@users.noreply.github.com>
2023-07-26 12:41:14 +0530

    chngenc

--- src/App.js
+++ src/App.js
@@ -8,41 +8,81 @@ const skt = io('http://localhost:7000')
 function App() {
   // const [idx, setIdx] = useState("")
   const [val, setVal] = useState("")
+  const [string,setString]=useState("");
+  const [mess,setMess] = useState("");
+  const [id,setId]=useState("");
+  const [docid,setDocid]=useState("");
+  const [flag,setFlag]=useState(false);
 
-  const [mess,setMess] = useState("")
   useEffect(()=>{
     skt.on("connect",()=>{
       console.log("done")
     })          
   },[])
   
+  
 
 
-
-  skt.on("cur",(res)=>{
-    console.log(res);
-    setMess(res.value)
-   })
+  // skt.on("cur",(res)=>{
+  //   console.log(res);
+  //   setMess(res.value)
+  //   setId(res._id)
+  //  })
+  skt.on("sendDoc",(res)=>{
+   setDocid(res._id);
+   setFlag(true);
+  })
  
- const handleSubmit = ()=>{ 
+ const handleSubmit = (vall)=>{ 
   // const qr ={
   //   idx:idx,
   //   str:mess, 
   // }
   // skt.emit('querry',qr)
   const q ={
-    str:val
+    str:vall,
+    docid:docid
   }
   skt.emit("ins",q);
  }
+const handleEdit=()=>{
+setString(mess);
+}
+ const handleUpdate=()=>{
+  const querry={
+    str:string,
+    strId:id
 
+  }
+   skt.emit("update",querry);
+ }
  
+ const handleCreate=()=>{
+   skt.emit("creation");
+ }
+ const handlenString=()=>{
+    if(string.length)
+    handleSubmit(string);
+    setString("");
+ }
   return (
     <div className="App">
-      <h1>{mess}</h1>
-     {/* <input type="text" onChange={(e)=>{setIdx(e.target.value)}} value={idx} className='inp'/> */}
-     <input type="text" onChange={(e)=>{setVal(e.target.value)}} value={val} className='inp' />
-     <button  onClick={handleSubmit} className='btn'>Submit</button>
+      <h1>{docid}</h1>
+      
+
+     <button type="submit" className="btn"
+     onClick={handleCreate}
+     >Create New Modal </button>
+      {flag===true ?<textarea name="" id="" cols="30" rows="10"
+     onKeyUp={(e)=>{ if (e.key === " " ||
+     e.code === "Space"     
+ ) { 
+     handlenString();
+ 
+ }}} 
+ onChange={(e)=>{setString((e.target.value))}}
+
+    ></textarea> : "" }
     </div>
   );
 }
