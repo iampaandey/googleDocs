799d3b08fcf165cfbbf55f1ac221c527a680d101
Vimal Mishra <skk180509@gmail.com>
2023-07-26 11:10:12 +0530

    vimcommit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import {useEffect, useState} from "react"
 import './App.css';
 import {  io } from 'socket.io-client';
-const skt = io('https://ugfgeyf.onrender.com')
+import { createPortal } from "react-dom";
+const skt = io('http://localhost:7000')
 
 
 function App() {
-  const [idx, setIdx] = useState("")
+  // const [idx, setIdx] = useState("")
+  const [val, setVal] = useState("")
+
   const [mess,setMess] = useState("")
   useEffect(()=>{
     skt.on("connect",()=>{
@@ -16,27 +19,29 @@ function App() {
 
 
 
-  skt.on("cur",(str)=>{
-    console.log(str);
-    setMess(str)
+  skt.on("cur",(res)=>{
+    console.log(res);
+    setMess(res.value)
    })
  
  const handleSubmit = ()=>{ 
-  const qr ={
-    idx:idx,
-    str:mess, 
+  // const qr ={
+  //   idx:idx,
+  //   str:mess, 
+  // }
+  // skt.emit('querry',qr)
+  const q ={
+    str:val
   }
-  skt.emit('querry',qr)
+  skt.emit("ins",q);
  }
-//  const submitQ =()=>{
-//   console.log(qry)
-//   skt.emit("querry", qry)
-//  }
+
  
   return (
     <div className="App">
       <h1>{mess}</h1>
-     <input type="text" onChange={(e)=>{setIdx(e.target.value)}} value={idx} className='inp'/>
+     {/* <input type="text" onChange={(e)=>{setIdx(e.target.value)}} value={idx} className='inp'/> */}
+     <input type="text" onChange={(e)=>{setVal(e.target.value)}} value={val} className='inp' />
      <button  onClick={handleSubmit} className='btn'>Submit</button>
     </div>
   );
